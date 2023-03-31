b6b76994441e3fecb2430fe75843a06942091d34
Brandon Ortiz <Brandonortiz7821@gmail.com>
2023-03-31 14:42:00 -0400

    Finalized layout for program. Created text file.

--- main.py
+++ main.py
@@ -3,3 +3,10 @@
 # File Name: H5_Num_Process.py
 # Function: This program reads and writes to files.
 
+# Read all numbers from the file and display them to the screen
+
+# Count the amount of numbers stored in the file and display to the screen
+
+# Calculate the sum of all the numbers in the file and display it to the screen
+
+# Write the results from the prior functions (amount, sum, average) to a file
